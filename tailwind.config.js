0480d0897154a8b403e9aaab86883200280bc92d
Gaurav Patil <gp949958@gmail.com>
2026-02-08 18:00:00 +0530

    Upload Project: Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Sarvat mavaiche!
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
